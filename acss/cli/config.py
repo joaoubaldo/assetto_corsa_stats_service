c1ac80f43b973c7239b11f008321abae1c3edac6
Joao Coutinho <me@joaoubaldo.com>
2016-06-13 22:14:33 +0100

    - fix: added missing module

---
+++ acss/cli/config.py
@@ -0,0 +1,18 @@
+import json
+
+
+def defaults():
+    settings = {
+        'db_filename': './acss.db',
+        'server_host': 'localhost',
+        'server_port': 8081,
+        'udp_bind_port': 10000,
+        'udp_remote_port': 10001
+    }
+    return settings
+
+
+def merge(json_file):
+    settings = defaults()
+    settings.update(json.loads(open(json_file).read()))
+    return settings
