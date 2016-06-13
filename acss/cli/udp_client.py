4992413004b5ce9a840ac8dad08c610edfbeb1a3
Joao Coutinho <me@joaoubaldo.com>
2016-06-13 22:20:34 +0100

    - fix: cli script - fix: config example updated

--- acss/cli/udp_client.py
+++ acss/cli/udp_client.py
@@ -1,19 +1,17 @@
 import time
+import sys
 
 from acss.udp.daemon import ACUDPDaemon
 from acss.cli import config
 
 
-def run(settings):
-    session = ACUDPDaemon(settings)
-
-    while 1:
-        session.update()
-        time.sleep(0.005)
-
-if __name__ == '__main__':
+def run():
     if len(sys.argv) != 2:
         sys.stderr.write("Usage: %s <config_file>\n" % (sys.argv[0],))
         sys.exit(1)
 
-    run(config.merge(sys.argv[1]))
+    session = ACUDPDaemon(config.merge(sys.argv[1]))
+
+    while 1:
+        session.update()
+        time.sleep(0.005)
